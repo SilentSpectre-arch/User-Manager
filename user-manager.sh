86a5f311d3a407719729aec43d8633317d8a1e97
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-07-12 22:49:07 +0330

    Addition of the menu and the add user function

---
+++ user-manager.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+echo "=============================="
+echo
+echo "	   User Manager Program     "
+echo
+echo "=============================="
+echo
+echo "1)Add User"
+echo
+echo "2)Delete User"
+echo
+echo "3)Lock User"
+echo
+echo "4)Unlock User"
+echo
+echo "5)Change Password"
+echo
+echo "6)User Info"
+echo
+echo "7)Lists User"
+echo
+echo "8)Exit"
+
+read -p "Select an option: " select
+
+add_user() {
+	while true;do
+		read -p "Enter Username: " username
+		
+		if id "$username" > dev/null 2>&1;then
+			echo "Error: "$username" already exist"
+			break
+		else
+			sudo adduser "$username"
+			echo "Success: User $username created successfully"
+			break
+		fi
+
+	done
+}
+			
+
+
